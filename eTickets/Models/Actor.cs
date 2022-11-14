54d74b9802b81dd7267cd061a060a3f5782ebe76
Manish Yogi <sspl.mansih2022@gmail.com>
2022-11-14 19:02:22 +0530

    Updated

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -13,8 +13,11 @@ namespace eTickets.Models
         [Display(Name = "Profile Picture")]
         public String ProfilePictureUrl { get; set; }
         [Display(Name = "Full Name")]
+        [Required(ErrorMessage ="Please Enter Full Name")]
         public string FullName { get; set; }
         [Display(Name = "Biography")]
+        [Required(ErrorMessage ="Please Enter Biography")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage ="Please Enter Text Between 3 to 50")]
         public string Bio { get; set; }
         //Relationships
         public List<Actor_Movie> Actor_Movies { get; set; }
